9697f3697d9783255277e1326d57a24b34e7b0de
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-03-01 19:47:24 +0530

    WellcomCalc unit testing

---
+++ src/components/logics/__test__/Wellcome.test.jsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import WellcomeCalc from "../../WellcomeCalc";
+
+describe('calculator text card should be there', () => {
+  
+  test('heading should be there', ()=>{
+    render(<WellcomeCalc />);
+    const calcHeadding = screen.getByRole("heading")
+    expect(calcHeadding).toBeInTheDocument();
+  })
+
+  test('heading should be there', ()=>{
+    render(<WellcomeCalc />);
+    const calcPara = screen.getByText("Start with some basics calculations")
+    expect(calcPara).toBeInTheDocument();
+  })
+
+  test('heading should be there', ()=>{
+    render(<WellcomeCalc />);
+    const calcPara = screen.getByText("How about 2 + 2 ?")
+    expect(calcPara).toBeInTheDocument();
+  })
+})
