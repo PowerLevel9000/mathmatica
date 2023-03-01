5832bd6ccb37204b467366eff638a1885f259e6d
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-03-01 20:07:51 +0530

    default unit testing

--- src/components/Default.jsx
+++ src/components/Default.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Default = () => (
-  <section className="page_404">
+  <section role="section" className="page_404">
     <div className="wrapper404">
       <h3 className="h2">
         Look like you&apos;re lost
       </h3>
       <p>the page you are looking for not avaible!</p>
-      <Link to="/" class="link_404">Go to Home</Link>
+      <Link to="/" className="link_404">Go to Home</Link>
     </div>
-
   </section>
 );
 
